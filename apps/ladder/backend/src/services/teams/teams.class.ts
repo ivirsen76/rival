e4915ea9dfc38de6d5cab98c37d4e825372e1595
Igor Eremeev <eremeev@3cisd.com>
2026-01-23 11:16:47 -0500

    Fix type issues with services classes

--- apps/ladder/backend/src/services/teams/teams.class.ts
+++ apps/ladder/backend/src/services/teams/teams.class.ts
@@ -1,37 +1,3 @@
-/* eslint-disable no-unused-vars */
-export const Teams = class Teams {
-    constructor(options) {
-        this.options = options || {};
-    }
+import { Service } from 'feathers-sequelize';
 
-    async find(params) {
-        return [];
-    }
-
-    async get(id, params) {
-        return {
-            id,
-            text: `A new message with ID: ${id}!`,
-        };
-    }
-
-    async create(data, params) {
-        if (Array.isArray(data)) {
-            return Promise.all(data.map((current) => this.create(current, params)));
-        }
-
-        return data;
-    }
-
-    async update(id, data, params) {
-        return data;
-    }
-
-    async patch(id, data, params) {
-        return data;
-    }
-
-    async remove(id, params) {
-        return { id };
-    }
-};
+export const Teams = class Teams extends Service {};
