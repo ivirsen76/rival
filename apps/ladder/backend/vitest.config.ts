16d212f1a2f060005f6e815ee0125d9b0ccbd3ea
Igor Eremeev <eremeev@3cisd.com>
2026-01-22 20:10:02 -0500

    Set NODE_CONFIG_DIR for vitest

--- apps/ladder/backend/vitest.config.ts
+++ apps/ladder/backend/vitest.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vitest/config';
+import path from 'path';
 
 export default defineConfig({
     test: {
         globals: true,
         environment: 'node',
         exclude: ['dist', 'build', 'node_modules'],
+        env: {
+            NODE_CONFIG_DIR: path.join(__dirname, 'dist', 'config'),
+        },
     },
 });
