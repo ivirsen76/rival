6b09f5702cca1c814eed28b52aa75eb9049a08c8
Igor Eremeev <eremeev@3cisd.com>
2026-01-26 14:40:04 -0500

    Adjust eslitn config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,13 +3,12 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
-import { defineConfig, globalIgnores } from 'eslint/config';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-    globalIgnores(['dist']),
+    { ignores: ['**/dist/**', '**/build/**', '**/avataaars/**', '**/metronic/**'] },
     {
         files: ['**/*.{ts,tsx,js,jsx}'],
-        ignores: ['**/dist/**', 'packages/metronic/**'],
         extends: [
             js.configs.recommended,
             tseslint.configs.recommended,
@@ -18,13 +17,31 @@ export default defineConfig([
         ],
         languageOptions: {
             ecmaVersion: 2020,
-            globals: globals.browser,
+            globals: {
+                React: 'readonly',
+                ...globals.browser,
+                ...globals.node,
+                ...globals.vitest,
+            },
         },
         rules: {
-            '@typescript-eslint/no-unused-vars': ['error', { args: 'none' }],
-            '@typescript-eslint/no-explicit-any': 'warn',
+            // Remove this block when all TS files will be checked
+            'no-undef': 'error',
+            'no-unused-vars': ['error', { args: 'none' }],
+            'no-redeclare': 'error',
+            'no-use-before-define': ['error', { functions: false }],
+
             '@typescript-eslint/ban-ts-comment': 'warn',
+            '@typescript-eslint/no-explicit-any': 'warn',
             '@typescript-eslint/no-unsafe-function-type': 'warn',
+            '@typescript-eslint/no-unused-expressions': 'off',
+            '@typescript-eslint/no-unused-vars': ['error', { args: 'none' }],
+            'react-hooks/exhaustive-deps': 'off',
+            'react-hooks/immutability': 'off',
+            'react-hooks/preserve-manual-memoization': 'off',
+            'react-hooks/purity': 'off',
+            'react-hooks/set-state-in-effect': 'off',
+            'react-refresh/only-export-components': 'off',
         },
     },
 ]);
