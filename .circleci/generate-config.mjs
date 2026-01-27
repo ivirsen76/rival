fec42cfcf6449972c811d33e989d491787223bb4
Igor Eremeev <eremeev@3cisd.com>
2026-01-27 18:11:19 -0500

    Fix path

--- .circleci/generate-config.mjs
+++ .circleci/generate-config.mjs
@@ -13,4 +13,4 @@ if (/^ladder@/.test(tag)) {
     source = 'ladder-config.yml';
 }
 
-fs.copyFileSync(path.join(__dirname, source), destination);
+fs.copyFileSync(path.join(__dirname, source), path.join(__dirname, destination));
