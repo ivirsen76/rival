6b78ac443a5703b5409b4f55dc2a00155886109e
Igor Eremeev <eremeev@3cisd.com>
2026-01-27 16:38:06 -0500

    Use current file folder

--- .circleci/generate-config.mjs
+++ .circleci/generate-config.mjs
@@ -1,4 +1,9 @@
 import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const tag = process.env.CIRCLE_TAG || '';
 const destination = 'generated_config.yml';
@@ -8,4 +13,4 @@ if (/^ladder@/.test(tag)) {
     source = 'ladder-config.yml';
 }
 
-fs.copyFileSync(source, destination);
+fs.copyFileSync(path.join(__dirname, source), path.join(__dirname, destination));
