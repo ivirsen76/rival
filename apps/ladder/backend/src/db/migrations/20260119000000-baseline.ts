42d6f6430d6936b46357ec5c92417fa7f7aafca9
Igor Eremeev <eremeev@3cisd.com>
2026-01-27 20:15:14 -0500

    Do not run first migration on production

--- apps/ladder/backend/src/db/migrations/20260119000000-baseline.ts
+++ apps/ladder/backend/src/db/migrations/20260119000000-baseline.ts
@@ -1,7 +1,17 @@
 import path from 'path';
 import { execSync } from 'child_process';
+import { QueryInterface } from 'sequelize';
 
-export const up = async () => {
+export const up = async (queryInterface: QueryInterface) => {
+    // Do not run migration if there are other migrations already
+    // It will not ruin the production
+    const [[row]] = (await queryInterface.sequelize.query(`SELECT count(*) AS cnt FROM _migrations`)) as [
+        { cnt: number },
+    ][];
+    if (row.cnt > 0) {
+        return;
+    }
+
     const { TL_DB_NAME, TL_DB_HOSTNAME, TL_DB_USERNAME, TL_DB_PASSWORD } = process.env;
     const dbCredentials = ` -h ${TL_DB_HOSTNAME} -u ${TL_DB_USERNAME} `;
     const dumpPath = path.join(__dirname, 'baseline.sql');
