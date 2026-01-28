904f6670c6c0e5bdf0914dcd32ac6f78189a9812
Igor Eremeev <eremeev@3cisd.com>
2026-01-28 12:28:09 -0500

    Get right path to gitcommit

--- apps/ladder/backend/src/gitcommit.ts
+++ apps/ladder/backend/src/gitcommit.ts
@@ -3,7 +3,7 @@ import path from 'path';
 
 const hash = (() => {
     try {
-        return fs.readFileSync(path.resolve(__dirname, '..', '..', '..', 'gitcommit'), 'utf8').slice(0, 10);
+        return fs.readFileSync(path.resolve(__dirname, '..', 'gitcommit'), 'utf8').slice(0, 10);
     } catch {
         return null;
     }
