0e49768c7730594cc3fe72f3f642b70b41c08032
Igor Eremeev <eremeev@3cisd.com>
2026-01-29 14:37:01 -0500

    Fix dynamic import

--- packages/common/components/Chart/index.tsx
+++ packages/common/components/Chart/index.tsx
@@ -1,6 +1,6 @@
 import loadable from '../../utils/loadable';
 
 // Just to preload lazy bundles
-const Chart = loadable(() => import(/* webpackPrefetch: true */ './lazy.jsx'), { fallback: null });
+const Chart = loadable(() => import('./lazy'), { fallback: null });
 
 export default (props) => <Chart {...props} />;
