7b004abecfc791a2d542f3c93eaa15198416495c
Igor Eremeev <eremeev@3cisd.com>
2026-01-30 13:58:45 -0500

    Simplify backend dayjs

--- apps/ladder/backend/src/utils/dayjs/index.ts
+++ apps/ladder/backend/src/utils/dayjs/index.ts
@@ -9,9 +9,8 @@ import isoWeek from 'dayjs/plugin/isoWeek';
 import isBetween from 'dayjs/plugin/isBetween';
 import customParseFormat from 'dayjs/plugin/customParseFormat';
 
-const isNode = Boolean(process.env.TL_DB_NAME);
 const TIMEZONE = process.env.TL_TIMEZONE;
-if (isNode && !TIMEZONE) {
+if (!TIMEZONE) {
     throw new Error('Timezone is not set in env variables');
 }
 
@@ -24,29 +23,6 @@ dayjs.extend(isSameOrBefore);
 dayjs.extend(isoWeek);
 dayjs.extend(isBetween);
 dayjs.extend(customParseFormat);
-if (TIMEZONE) {
-    dayjs.tz.setDefault(TIMEZONE);
-}
-
-export const formatLong = (date: string) => {
-    const days = {
-        0: 'Sun',
-        1: 'Mon',
-        2: 'Tue',
-        3: 'Wed',
-        4: 'Thu',
-        5: 'Fri',
-        6: 'Sat',
-    };
-
-    const localDate = dayjs.tz(date);
-    const day = localDate.day();
-
-    return days[day] + localDate.format(', MMM D, h:mm A');
-};
-export const formatMiddle = (date: string) => dayjs.tz(date).format('MMM D, h:mm A');
-export const formatShort = (date: string) => dayjs.tz(date).format('h:mm A');
-export const formatDate = (date: string) => dayjs.tz(date).format('MMM\xa0D, YYYY');
-export const formatCustom = (date: string, str: string) => dayjs.tz(date).format(str);
+dayjs.tz.setDefault(TIMEZONE);
 
 export default dayjs;
