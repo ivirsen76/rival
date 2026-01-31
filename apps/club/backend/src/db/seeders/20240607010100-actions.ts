496c5a33ff772570ae2342777842ed8a5ac3e122
Igor Eremeev <eremeev@3cisd.com>
2026-01-31 16:30:16 -0500

    Delete dist folder before building files

--- apps/club/backend/src/db/seeders/20240607010100-actions.ts
+++
@@ -1,8 +0,0 @@
-import { QueryInterface } from 'sequelize';
-
-export const up = async ({ context: queryInterface }: { context: QueryInterface }) => {
-    // clean actions, including "generateBadges" action
-    await queryInterface.sequelize.query('DELETE FROM actions');
-};
-
-export const down = async ({ context: queryInterface }: { context: QueryInterface }) => {};
